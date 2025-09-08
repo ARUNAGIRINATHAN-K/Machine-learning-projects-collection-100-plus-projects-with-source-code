77536e869c39d1c37a0ceb654a45ec8ededb24e3
DEVELOPER <arunagirinathan06@outlook.com>
2025-09-08 16:11:42 +0530

    Delete Text and Image Processing directory

--- Text and Image Processing/SMS Spam Detection using TensorFlow in Python/text.py
+++
@@ -1,8 +0,0 @@
-# Splitting data for Training and testing
-from sklearn.model_selection import train_test_split
-
-X, y = np.asanyarray(df['Text']), np.asanyarray(df['label_enc'])
-new_df = pd.DataFrame({'Text': X, 'label': y})
-X_train, X_test, y_train, y_test = train_test_split(
-    new_df['Text'], new_df['label'], test_size=0.2, random_state=42)
-X_train.shape, y_train.shape, X_test.shape, y_test.shape
